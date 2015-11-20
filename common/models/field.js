d8174ff37f11faeab5c2ed8aa5de47c3f93c05e2
Jose Lloret <jose.lloret@ed.ac.uk>
2015-11-20 12:38:16 +0000

    record model checking fields property according to the Field model

---
+++ common/models/field.js
@@ -0,0 +1,4 @@
+module.exports = function(Field) {
+	Field.validatesInclusionOf('type',{in: ['Text','Range','TextArea','Audio','Check','Image','Option','Radio'], 
+											message: 'Text, Range, TextArea, Audio, Check, Image, Option or Radio are the valid types.'});
+};
--- common/models/record.js
+++ common/models/record.js
@@ -1,3 +1,29 @@
+var app = require('../../server/server'); 
 module.exports = function(Record) {
-
+	Record.observe('before save',function(ctx,next){
+		if(ctx.instance !== undefined){	//create(), findOrCreate() or prototype.save()
+			var record = ctx.instance;
+			var fields = record.properties.fields;
+			var field = undefined;
+			for(var i=0;i<fields.length;i++){
+				app.models.Field.create(fields[i],function(err,field){
+					if(err !== null)
+						next(err);	//error trying to create an instance of Field
+					else{
+						field.isValid(function(valid){
+							if(valid){
+								if(field.type === 'Audio' || field.type === 'Image'){
+									//Search if exists any ObjectId asset with the value of the field.val
+								}
+							}
+							else{	//Error validating the field instance
+								next(new ValidationError(field));	//creates a ValidationError passing the field instance failed after validating
+							}
+						});
+					}
+				});
+			}
+			next();
+		}
+	});
 };
