a7dfe27eb49318336007f965c2de75a8763c86bd
Jose Lloret <jose.lloret@ed.ac.uk>
2015-11-20 17:15:25 +0000

    dataSource connection programatically

--- common/models/asset.js
+++ common/models/asset.js
@@ -1,16 +1,29 @@
 module.exports = function(Asset) {
-	//REMOTE METHOD EXAMPLE
-	Asset.fileSize = function(cb){
-		var size = 100;
-		cb(null,'The maximum filesize is: '+size+' MB');
+	var DataSource = require('loopback-datasource-juggler').DataSource;
+	var storageMongo = new DataSource({
+		connector: require('loopback-component-storage-mongo'),
+		host: 'localhost',
+		port: 27017,
+		database: 'fieldtripGB'
+	});
+	Asset.attachTo(storageMongo);
+	Asset.findById = function(id,cb){
+		cb(null,{status:'OK'});
+		/*
+		return Asset.db.collection('fs.files').findOne({
+			'_id': id
+		}, function(err,file){
+			if(err)
+				cb(err);
+			if(!file){
+				err = new Error('File not found');
+				err.status = 404;
+				cb(err);
+			}
+			cb(null,file);
+		});
+		*/
 	};
-	Asset.remoteMethod(
-		'fileSize',{
-			accepts:[],
-			returns: {arg: 'message', type: 'string'},
-			http: {path: '/file-size', verb: 'get'}
-		}
-	);
 	Asset.remoteMethod(
 		'findById',{
 			accepts:[{arg:'id',type:'string'}],
@@ -19,4 +32,3 @@ module.exports = function(Asset) {
 		}
 	);
 };
-
